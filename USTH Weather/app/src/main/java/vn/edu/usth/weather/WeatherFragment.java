2169e0c08baf88d8034c23d56d2ef053e3d80b61
AnhNguyen <anhqfwork@gmail.com>
2021-10-13 02:04:43 +0700

    Practical Work 5-6

--- USTH Weather/app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
+++ USTH Weather/app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
@@ -1,6 +1,5 @@
 package vn.edu.usth.weather;
 
-import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,19 +7,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
-public class ForecastFragment extends Fragment {
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link WeatherFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WeatherFragment extends Fragment {
 
     View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-         view = inflater.inflate(R.layout.fragment_forecast, container, false);
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_weather, container, false);
 
-         return view;
+        return view;
     }
 }
