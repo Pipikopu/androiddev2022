3c6f3419d5799dea30d3a37b951c4c251914e1f1
AnhNguyen <anhqfwork@gmail.com>
2021-10-13 04:47:07 +0700

    Practical Work 12

--- USTH Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTH Weather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,11 +1,15 @@
 package vn.edu.usth.weather;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager.widget.PagerAdapter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -25,6 +29,27 @@ public class WeatherActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
         tabLayout.setupWithViewPager(pager);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_weather, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.refresh:
+                recreate();
+                return true;
+            case R.id.setting:
+                Intent intent = new Intent(WeatherActivity.this, PrefActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
